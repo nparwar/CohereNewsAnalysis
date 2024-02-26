4c1d3340b0204a1c4b622e5273bba5906687d8cf
nparwar <123691454+nparwar@users.noreply.github.com>
2024-02-26 00:50:15 -0600

    Successfully Pulled NYT Articles

--- analysis.py
+++ analysis.py
@@ -1,33 +1,5 @@
 import streamlit as st
-import os
-import requests
-import time
 from retrieve import get_NYT
-
-#Get NYT Articles
-def get_NYT(name, date="Today"):
-
-    api_key = os.getenv("NYT_API_KEY")
-    base_url = "https://api.nytimes.com/svc/search/v2/articlesearch.json"
-    query = name
-    headline_filter = f'headline:({name})'
-    page_limit = 10  
-    article_list = []
-    for page in range(page_limit):
-        params = {
-            "q": query,
-            "fq": headline_filter,
-            "api-key": api_key,
-            "page": page
-        }
-        response = requests.get(base_url, params=params)
-        article = response.json()["response"]["docs"]["headline"]["main"]
-        if not article:
-            break
-        article_list.append(article)
-        time.sleep(12)
-    
-    return article_list
 
 
 def main():
@@ -49,7 +21,7 @@ def main():
     
     #Start Analysis
     if search_button and company_name:
-        articles = get_news_articles(company_name)
+        articles = get_NYT(company_name)
         st.write(articles)
 
 
--- retrieve.py
+++ retrieve.py
@@ -1,14 +1,15 @@
 import os
 import requests
 import time
+from dotenv import load_dotenv
 
 def get_NYT(name, date="Today"):
-
+    load_dotenv()
     api_key = os.getenv("NYT_API_KEY")
     base_url = "https://api.nytimes.com/svc/search/v2/articlesearch.json"
     query = name
     headline_filter = f'headline:({name})'
-    page_limit = 10  
+    page_limit = 5  
     article_list = []
     for page in range(page_limit):
         params = {
@@ -18,10 +19,12 @@ def get_NYT(name, date="Today"):
             "page": page
         }
         response = requests.get(base_url, params=params)
-        article = response.json()["response"]["docs"]["headline"]["main"]
-        if not article:
+        articles = response.json()["response"]["docs"]
+        if not articles:
             break
-        article_list.append(article)
+        for article in articles:
+            headline = article["headline"]["main"]
+            article_list.append(headline)   
         time.sleep(12)
     
     return article_list
